d6e8f25526c234f6fe2b2fb2a836fb7b0d884479
Caroline Belmas <caroline.belmas@gmail.com>
2021-03-09 15:34:08 +0100

    exercice 2 Dog Years done

---
+++ exercice_2_dog_years.js
@@ -0,0 +1,8 @@
+const myAge = 41; //Create a variable named myAge, and set it equal to your age as a number.
+let earlyYears = 2; //Create a variable named earlyYears and save the value 2 to it
+earlyYears *= 10.5; //multiply the value saved to earlyYears by 10.5 and reassign it to earlyYears
+let laterYears = myAge - 2; // Create a variable laterYears and set it equal to 2 substract from myAge
+laterYears *= 4; //Multiply the laterYears variable by 4 to calculate the number of dog years accounted for by your later years
+const myAgeInDogYears = earlyYears + laterYears //Add earlyYears and laterYears together, and store that in a variable named myAgeInDogYears
+const myName = 'Caroline'.toLowerCase() //write my name in a variable myName and putt it in lowercase letters
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`) //displays your name and age in dog years
